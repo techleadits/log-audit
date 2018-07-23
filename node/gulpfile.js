3fe3959cd511b9fb964aec4ad1a34f1dd7f34787
Lourenco Viana <lourenco.viana@techlead.local>
2018-07-23 11:03:00 -0300

    adicionado suporte para build de node no gradle

---
+++ node/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+
+// define tasks here
+gulp.task('default', function(){
+  // run tasks here
+  // set up watch handlers here
+});
+
+
+gulp.task('genver', function(){
+    // run tasks here
+    // set up watch handlers here
+});
+  
